904e63ea1cac4e0c985db31a9ace9a53f8b90f65
Christoffer Hauthorn <christoffer@emplate.it>
2016-05-05 09:33:21 +0200

    Added image activity and menu for MainActivity

---
+++ app/src/main/java/com/example/winther/densiometer/ImageCalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.winther.densiometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_calculator);
+    }
+}
--- app/src/main/java/com/example/winther/densiometer/MainActivity.java
+++ app/src/main/java/com/example/winther/densiometer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.winther.densiometer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_photo:
+                // User chose the "Settings" item, show the app settings UI...
+                Intent intent = new Intent(this, ImageCalculatorActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
